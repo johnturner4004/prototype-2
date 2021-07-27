fecd3b9abaac7e3f44d6a5f231f9d8e8c24ff378
johnturner4004 <johnturner4004@gmail.com>
2021-07-27 13:29:59 -0400

    make player throw bone when space bar is pressed

--- Assets/Scripts/Player01Control.cs
+++ Assets/Scripts/Player01Control.cs
@@ -6,6 +6,10 @@ public class Player01Control : MonoBehaviour
 {
   public float horizontalInput;
   public float speed = 10.0f;
+  //sets max/min x values
+  public float xRange = 20.0f;
+
+  public GameObject ProjectilePrefab;
   // Start is called before the first frame update
   void Start()
   {
@@ -15,7 +19,28 @@ public class Player01Control : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    horizontalInput = Input.GetAxis("Horizontal");
-    transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+    //prevents player from falling off map
+    if (transform.position.x <= -xRange)
+    {
+      transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+    }
+
+    if (transform.position.x >= xRange)
+    {
+      transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+    }
+
+
+    {
+      // horizontal controlls
+      horizontalInput = Input.GetAxis("Horizontal");
+      transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+    }
+
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      // launch a projectile from the player
+      Instantiate(ProjectilePrefab, transform.position, ProjectilePrefab.transform.rotation);
+    }
   }
 }
