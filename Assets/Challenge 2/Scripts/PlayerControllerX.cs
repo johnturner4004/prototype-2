069c48b8ea7274b9e42b8827166cd1cb766a6830
johnturner4004 <johnturner4004@gmail.com>
2021-07-31 22:20:49 -0400

    limit fire rate

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public GameObject dogPrefab;
+  public GameObject dogPrefab;
+  public bool allowDog = true;
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    // On spacebar press, send dog
+    if (Input.GetKeyDown(KeyCode.Space) && allowDog)
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
-        }
+      StartCoroutine(Dog());
+      Debug.Log("Dog sent!");
     }
+  }
+
+  IEnumerator Dog()
+  {
+    allowDog = false;
+    Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    yield return new WaitForSeconds(1.0f);
+    allowDog = true;
+  }
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
 {
-    public GameObject[] ballPrefabs;
+  public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = -22;
-    private float spawnLimitXRight = 7;
-    private float spawnPosY = 30;
+  private float spawnLimitXLeft = -22;
+  private float spawnLimitXRight = 7;
+  private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+  private float startDelay = 1.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    float spawnInterval = Random.Range(3.0f, 5.0f);
+    InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+  }
 
-    // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
-    {
-        // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+  // Spawn random ball at random x position at top of play area
+  void SpawnRandomBall()
+  {
+    // Generate random ball index and random spawn position
+    Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
-    }
+    // instantiate ball at random spawn location
+    Instantiate(ballPrefabs[Random.Range(0, 3)], spawnPos, ballPrefabs[Random.Range(0, 3)].transform.rotation);
+  }
 
 }
